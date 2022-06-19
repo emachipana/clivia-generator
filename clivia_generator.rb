d11b40157bd60d794463eb1b0823d461a0a91952
emachipana <emachipana0102@gmail.com>
2022-06-18 23:32:07 -0500

    ADD: start

--- clivia_generator.rb
+++ clivia_generator.rb
@@ -1,6 +1,6 @@
 require_relative "requester"
-require_relative "presenter"
-require_relative "queries_api"
+require_relative "display"
+require_relative "fetch-api"
 require "colorize"
 require "json"
 require "terminal-table"
@@ -15,6 +15,22 @@ class CliviaGenerator
     @filename = ARGV.shift
     @filename = "scores.json" if @filename.nil?
     @score = 0
+  end
+
+  def start
+    File.open(filename, "a")
+    action = ""
+    until action == "exit"
+      puts welcome.join("\n").colorize(color: :light_red, mode: :bold)
+      puts menu_action(:main_menu)
+      action = get_inputs(menu_action(:main_menu))
+      case action
+      when "random" then random_trivia
+      when "scores" then puts print_scores
+      when "exit" then puts bye.join("\n").colorize(color: :light_red, mode: :bold)
+      else puts "Please type a valid option".colorize(color: :red, background: :light_white, mode: :bold)
+      end
+    end
   end
 
 end
