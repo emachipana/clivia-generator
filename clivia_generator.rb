e7928bd4ba185040b38e9f6f3f32834290e3cac3
emachipana <emachipana0102@gmail.com>
2022-06-18 23:34:18 -0500

    ADD: generate quiestions

--- clivia_generator.rb
+++ clivia_generator.rb
@@ -33,4 +33,41 @@ class CliviaGenerator
     end
   end
 
+  def random_trivia
+    @score = 0
+    questions = Fetch.load_questions
+    i = 1
+    questions.each do |question|
+      puts "#{i}.Category: #{question[:category]} | Difficulty:  #{question[:difficulty]}".colorize(color: :blue,
+                                                                                                    mode: :bold)
+      puts "  Question: #{question[:question]}".colorize(color: :blue, mode: :bold)
+      ask_questions(question)
+      i += 1
+    end
+    system("clear")
+    puts "Well done! Your score is #{score}".colorize(color: :green, mode: :bold)
+    save_confirm
+  end
+
+  def ask_questions(question)
+    answers = load_answers(question)
+    i = 1
+    options = []
+    answers.each do |value|
+      puts "  #{i}. #{value}".colorize(color: :yellow, mode: :bold)
+      options.push(i.to_s)
+      i += 1
+    end
+    answer = get_inputs(options)
+    correct_answer = question[:correct_answer].is_a?(Array) ? question[:correct_answer].join(" ") : question[:correct_answer]
+    if answers[answer.to_i - 1] == correct_answer
+      puts "  Well done!".colorize(color: :magenta, mode: :bold)
+      @score += 10
+    else
+      puts "  #{answers[answer.to_i - 1]}... Incorrect!\n  The correct answer was: #{correct_answer}".colorize(
+        color: :red, mode: :bold
+      )
+    end
+  end
+
 end
