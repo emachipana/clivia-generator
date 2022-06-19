625d02f18fbca3b90d33790a99a69efe3237b001
emachipana <emachipana0102@gmail.com>
2022-06-18 23:30:47 -0500

    ADD: initialize class clivia generator

--- clivia_generator.rb
+++ clivia_generator.rb
@@ -4,3 +4,17 @@ require_relative "queries_api"
 require "colorize"
 require "json"
 require "terminal-table"
+
+class CliviaGenerator
+  include Requester
+  include Presenter
+  include Queries
+
+  attr_accessor :filename, :score
+  def initialize
+    @filename = ARGV.shift
+    @filename = "scores.json" if @filename.nil?
+    @score = 0
+  end
+
+end
