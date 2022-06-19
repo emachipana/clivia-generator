67787451dcf02de0fba026099d0650c71a051079
emachipana <emachipana0102@gmail.com>
2022-06-18 23:24:44 -0500

    ADD: method to load answers

--- requester.rb
+++ requester.rb
@@ -27,7 +27,7 @@ module Requester
     input
   end
 
-  def save
+  def save_confirm
     puts "-" * 50
     puts "Do you want to save your score? (y/n)".colorize(mode: :bold, color: :green)
     confirm = get_inputs("Y y N n")
@@ -37,8 +37,21 @@ module Requester
       name = gets.chomp
       name = name.empty? ? "Anonymous" : name.capitalize
       data = { name: name, score: score }
-      save(data)
+      save(data) # method in clivia_generator.rb
     end
+  end
+
+  def load_answers(question)
+    answers = []
+    correct_answer = question[:correct_answer].is_a?(Array) ? question[:correct_answer].join(" ") : question[:correct_answer]
+    first_incorrect_answer = question[:incorrect_answers][0].is_a?(Array) ? question[:incorrect_answers][0].join(" ") : question[:incorrect_answers][0]
+    second_incorrect_answer = question[:incorrect_answers][1].is_a?(Array) ? question[:incorrect_answers][1].join(" ") : question[:incorrect_answers][1]
+    third_incorrect_answer = question[:incorrect_answers][2].is_a?(Array) ? question[:incorrect_answers][2].join(" ") : question[:incorrect_answers][2]
+    answers.push(correct_answer) unless question[:correct_answer].nil?
+    answers.push(first_incorrect_answer) unless question[:incorrect_answers][0].nil?
+    answers.push(second_incorrect_answer) unless question[:incorrect_answers][1].nil?
+    answers.push(third_incorrect_answer) unless question[:incorrect_answers][2].nil?
+    answers.sort_by! { rand }
   end
 
 end
