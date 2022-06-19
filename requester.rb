ded38ef7e12a42f59bda67d292b0945c2b4bb2bf
emachipana <emachipana0102@gmail.com>
2022-06-18 23:20:09 -0500

    ADD: get inputs method

--- requester.rb
+++ requester.rb
@@ -5,4 +5,16 @@ module Requester
     }
     menus[option]
   end
+
+  def get_inputs(options)
+    print "> ".colorize(mode: :bold, color: :yellow)
+    input = gets.chomp
+    input = empty(input)
+    until options.include?(input)
+      puts "Please type a valid option".colorize(color: :red, background: :light_white, mode: :bold)
+      print "> ".colorize(mode: :bold, color: :yellow)
+      input = gets.chomp
+    end
+    input
+  end
 end
